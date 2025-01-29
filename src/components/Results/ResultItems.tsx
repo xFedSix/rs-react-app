d934a0a725d6aaa5717a55eaa5615ead67f45860
Fiodar Siyanka <siyankafiodar@gmail.com>
2025-01-29 18:27:36 +0300

    feat: add API fetch functionality

--- src/components/Results/ResultItems.tsx
+++ src/components/Results/ResultItems.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-interface ResultsProps {
-  items: Array<{ id: number; title: string; description: string }>;
+export interface Item {
+  id: number;
+  name: string;
+  description: string;
 }
-
+interface ResultsProps {
+  items: Item[];
+}
 const ResultsItem: React.FC<ResultsProps> = ({ items }) => {
   return (
     <div>
       {items.map((item) => (
         <div key={item.id}>
-          <h3>{item.title}</h3>
-
-          <p>{item.description}</p>
+          <h3>{item.id}</h3>
+          <p>{item.name}</p>
         </div>
       ))}
     </div>
